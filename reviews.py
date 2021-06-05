5f7775c561dd8e6c49f359fceb956bd209f9540a
roger tu <zongyi0520@gmail.com>
2021-06-05 11:37:43 +0800

    add filter function

--- reviews.py
+++ reviews.py
@@ -7,9 +7,9 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count +=1
-        if count % 100000 == 0:   # % 求餘數
+        if count % 500000 == 0:   # % 求餘數
             print(len(data))
-print('檔案讀取結束，留言總筆數= ', len(data))
+print('檔案讀取結束，留言總筆數 =', len(data))
 
 #sum = 0
 #for x in range(1000000):
@@ -20,6 +20,15 @@ print('檔案讀取結束，留言總筆數= ', len(data))
 sum = 0
 for d in data:
     sum = sum + len(d)
-average = sum / len(data)
-print('留言平均長度= ',average)
+print('留言平均長度= ', sum / len(data))
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+
+
+
 
