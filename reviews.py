94d654015d166e29eac79a49a1cc8e5ac2306b5d
roger tu <zongyi0520@gmail.com>
2021-06-05 11:23:28 +0800

    add third version by teacher

--- reviews.py
+++ reviews.py
@@ -11,9 +11,15 @@ with open('reviews.txt', 'r') as f:
             print(len(data))
 print('檔案讀取結束，留言總筆數= ', len(data))
 
+#sum = 0
+#for x in range(1000000):
+#   sum = sum + len(data[x])
+#averge = sum / 1000000
+#print('留言平均長度= ',averge)
+
 sum = 0
-for x in range(1000000):
-    sum = sum + len(data[x])
-averge = sum / 1000000
-print('留言平均長度= ',averge)
+for d in data:
+    sum = sum + len(d)
+average = sum / len(data)
+print('留言平均長度= ',average)
 
