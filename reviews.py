218a5b9b48c564232ae4fe90c15a0bb55b084a0f
roger tu <zongyi0520@gmail.com>
2021-06-05 12:08:17 +0800

    add fast list gen method

--- reviews.py
+++ reviews.py
@@ -29,13 +29,22 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 
-good = []
-for d in data:
-    if 'good' in d:
-        good.append(d)
-print('一共有', len(good), '筆留言提到good這個字')
-print(good[1])
+# good = []
+# for d in data:
+#     if 'good' in d:
+#         good.append(d)
+# print('一共有', len(good), '筆留言提到good這個字')
+# print(good[1])
 
+#good = [d for d in data if 'good' in d]  　#這行的篩選是上面的快寫法 這是原封不動把d的內容放進清單
+good = [1 for d in data if 'good' in d]  #這是變成要把1裝進去good的清單中
+print(good)
 
+#bad = ['bad' in d for d in data]
+bad = [d for d in data if 'bad' in d]
+print(len(bad))
 
-
+# bad = []
+# for d in data:
+#     bad.append('bad' in d)
+print(len(bad))
